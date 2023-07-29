798a7bb2b4bca57c6dcf9c04aacf7b6ee4f20c67
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-07-29 16:09:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+  let str = document.getElementById("rope-lengths").value.split(",");
+  let arr = str.map((element) =>
+	  {
+		  return parseInt(element);
+	  })
+  let ans = 0;
+while(arr.length!=1)
+{
+    arr.sort((a,b) => a-b);
+    const sum = arr[0]+arr[1];
+    ans += sum;
+    arr.splice(0,2);
+    arr.push(sum);
+}
+	const div = document.createElement("div");
+	div.innerText = ans;
+	document.body.appendChild(div);
 }  
